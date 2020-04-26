a99a69c58cc3921e218b34d9e674b74d59262974
xavi chavez <chvzalejandro@gmail.com>
2020-04-25 20:18:55 -0700

    Add images

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -9,15 +9,15 @@ export default class About extends Component {
       <body>
       <h1>About Us</h1>
       <h2>William</h2>
-        <img src="https://images.indianexpress.com/2017/11/trump-water-bottle-main.jpg?w=350"
+        <img src="./statics/William.JPG" className="will"
         alt="William" width="500" height="350">
         </img>
       <h2>Alejandro</h2>
-        <img src="https://www.washingtonpost.com/resizer/XVOI9bBsf_w2ujuFJ0RtBRNeV_0=/1440x0/smart/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/EWEJNPWIA45FXO6B75CID5VV4Q.png"
+        <img src="./statics/Alejandro.JPG"
         alt="Alejandro" width="500" height="350">
         </img>
       <h2>Juan</h2>
-          <img src="https://www.informador.mx/__export/1571061579580/sites/elinformador/img/2019/10/14/image_crop1571061132974.jpg_1034156727.jpg"
+          <img src="./statics/Juan.JPG"
           alt="Juan" width="500" height="350">
           </img>
       </body>
